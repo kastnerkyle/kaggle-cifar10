163c28680db841a2d096a206f986427ade2fb92c
Kyle Kastner <kastnerkyle@gmail.com>
2013-10-22 13:18:11 -0500

    Working version, but found possible bug

--- test_cifarpl2.py
+++ test_cifarpl2.py
@@ -2,29 +2,38 @@
 from pylearn2.utils import serial
 from pylearn2.datasets import cifar10
 from pylearn2.datasets import preprocessing
+from theano import tensor as T
+from theano import function
+import numpy as np
+
+
+trn = cifar10.CIFAR10('train',
+                      toronto_prepro=False,
+                      one_hot=True,
+                      axes=('c', 0, 1, 'b'))
 
 tst = cifar10.CIFAR10('test',
                       toronto_prepro=False,
                       one_hot=True,
                       axes=('c', 0, 1, 'b'))
 
-# preprocessor = preprocessing.ZCA()
-# tst.apply_preprocessor(preprocessor=preprocessor, can_fit=True)
+preprocessor = preprocessing.ZCA()
+trn.apply_preprocessor(preprocessor=preprocessor, can_fit=True)
+tst.apply_preprocessor(preprocessor=preprocessor, can_fit=True)
 
 mdl = serial.load('cifar10_maxout_zca.pkl')
-batch_size = 20
+# This batch size must be evenly divisible into number of total samples!
+batch_size = 100
 mdl.set_batch_size(batch_size)
 X = mdl.get_input_space().make_batch_theano()
 Y = mdl.fprop(X)
-
-from theano import tensor as T
-from theano import function
 y = T.argmax(Y, axis=1)
 f = function([X], y)
-y = []
+yhat = []
 for i in xrange(tst.X.shape[0] / batch_size):
     x_arg = tst.X[i * batch_size:(i + 1) * batch_size, :]
-    if X.ndim > 2:
-        x_arg = tst.get_topological_view(x_arg)
-    y.append(f(x_arg))
-print y
+    x_arg = tst.get_topological_view(x_arg)
+    yhat.append(f(x_arg))
+yhat = np.array(yhat)
+
+print "Test(%): ", (tst.y.argmax(axis=1).reshape(yhat.shape) == yhat).mean()
